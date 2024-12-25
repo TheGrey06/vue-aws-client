4bb17093bc7c4d212d296ed5a6b00a5f6a8838e3
GiuseppeAugello <fullstack.development.aug@gmail.com>
2024-12-25 18:07:01 +0000

    configured tailwind on the client

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,16 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-// export default {
-//   content: [],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// };
-
-// tailwind.config.js
 module.exports = {
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  darkMode: "media", // or 'media' or 'class'
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
