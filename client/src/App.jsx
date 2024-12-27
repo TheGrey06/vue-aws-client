e59c33fe079ebaec1bce21b611d143b850def191
GiuseppeAugello <fullstack.development.aug@gmail.com>
2024-12-27 19:36:29 +0000

    Implemented session for post one bucket route and rudimental front end

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,23 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate, Link } from "react-router-dom";
 import Login from "./pages/Login";
 import Buckets from "./pages/Buckets";
+import CreateBucket from "./pages/CreateBucket";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
     <Router>
+      <nav>
+        {isLoggedIn && (
+          <ul>
+            <li><Link to="/buckets">Buckets</Link></li>
+            <li><Link to="/create-bucket">Create Bucket</Link></li>
+            <li><button onClick={() => setIsLoggedIn(false)}>Log Out</button></li>
+          </ul>
+        )}
+      </nav>
       <Routes>
         <Route
           path="/login"
@@ -16,6 +26,10 @@ function App() {
         <Route
           path="/buckets"
           element={isLoggedIn ? <Buckets onLogout={() => setIsLoggedIn(false)} /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/create-bucket"
+          element={isLoggedIn ? <CreateBucket /> : <Navigate to="/login" />}
         />
         <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
